1756ec97bbfbddf94df318c015bdf4b64eb32bfb
iambasantarai <iambasantarai@gmail.com>
2024-11-08 20:13:38 +0545

    feat(invertColors): make the function more customizable using parameters

--- src/pranks/invertColors.ts
+++ src/pranks/invertColors.ts
@@ -1,19 +1,31 @@
-const invertColors = (): void => {
-  let delay = 0;
-  let duration = 3000;
+/**
+ * Inverts the color of the web page, controlled by passed parameters
+ *
+ * @param {number} initialDuration - Initial duration of how long the inversion should happen (in ms)
+ * @param {number} delay - Delay between each color inversion (in ms)
+ * @param {number} repetition - Number of times the inversion should repeat
+ * @returns {void}
+ */
+const invertColors = (
+  initialDuration: number = 3000,
+  delay: number = 0,
+  repetition: number = 3,
+): void => {
+  let duration = initialDuration;
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 1; i <= repetition; i++) {
     setTimeout(() => {
       document.body.style.filter = "invert(100%)";
 
-      // revert back to normal after the current duration
       setTimeout(() => {
         document.body.style.filter = "none";
       }, duration);
-
-      duration -= 1000;
     }, delay);
 
+    // decrease the duration after each repetition
+    duration = Math.max(1000, duration - 1000);
+
+    // increase the delay for the next iteration
     delay += duration + 1000;
   }
 };
